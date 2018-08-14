1dcc0d06165bfcca8dab0c62c88bcdaaf9b0bcb6
Paulson McIntyre <paul@gpmidi.net>
2018-08-13 22:57:53 -0400

    doc++

--- storage/s3.go
+++ storage/s3.go
@@ -2,6 +2,9 @@ package storage
 
 import "github.com/aws/aws-sdk-go/service/s3"
 
+// https://docs.aws.amazon.com/sdk-for-go/api/service/s3/
+import "github.com/aws/aws-sdk-go/service/s3"
+
 type ImageStore struct {
 	BucketName string
 	Bucket     s3.Bucket
